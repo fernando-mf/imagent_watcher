14196bfab303ab7d0adef88ffe6d01c2fe9f1cd7
Qlub53 <qlub53@gmail.com>
2023-02-12 12:40:55 +0800

    Supported GB and TB

--- watcher.py
+++ watcher.py
@@ -1,49 +1,45 @@
-import subprocess
 import re
+import subprocess
+from enum import Enum
+
 import psutil
+
+
+class Unit(Enum):
+  KB = "K"
+  MB = "M"
+  GB = "G"
+  TB = "T"
+
+
+class Multiplier(Enum):
+  KB = 1
+  MB = 1000 * KB
+  GB = 1000 * MB
+  TB = 1000 * GB
+
 
 # You can watch any process in my case it was the imagent because it had a memory leak in Mac OS Sierra
 PROCESS = "imagent"
-THRESHOLD = 100 # MB
-THRESHOLD = 2000
-UNIT = "M"
-UNIT = "K"
+UNIT = Unit.MB
+THRESHOLD = 100 * Multiplier[UNIT.name].value  # Normalize size unit to KB
 
-# Process list command, this will print the memory usage and the PID
-COMMAND = "top -o mem -l 1 | grep imagent | awk '{print $8 \" \" $1}'"
+# Process list command, this will print "$memory_usage $PID $proces_name"
+COMMAND = "top -o mem -l 1 | grep imagent | awk '{print $8 \" \" $1 \" \" $2}'"
 
 def killProcess(pid):
-  # Iterate over all running process
-  for proc in psutil.process_iter():
-    try:
-      # Get process name & pid from process object.
-      processName = proc.name()
-      processPID = str(proc.pid)
-
-      # If a match is found, then kill the process
-      if (processName == PROCESS) or (processPID == pid):
-        print("Trying to kill...")
-        proc.kill()
-        print("Killed ", processName, " with PID ", processPID)
-        return True
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as error:
-      print("Error: ", error)
-      return False
-
-  return False
+  process = psutil.Process(pid)
+  if process.name() == PROCESS:
+    process.kill()
+    print("Killed. pid={} pname={}".format(pid, process.name()))
+  return True
 
 # This runs until no more matches are found
 def runUntilKilled(pid):
   res = killProcess(pid)
   if not res:
     print("No process to kill")
-    return 
-
-  # Loop the processes and kill matches
-  while res:
-    res = killProcess(pid)
-
-  print("Process killed.")
+    return
 
 # Execute process list command
 output = subprocess.check_output(['bash','-c', COMMAND])
@@ -53,25 +49,38 @@ output = str(output).split(' ')
 
 memory = output[0]
 pid = output[1]
+pname = re.findall("(.*)\\\\n", output[2])[0]
 
 mem_regex = "(\d)"
-unit_regex = "([KMB])"
+unit_regex = "([KMGTB])"
+
+# Unit T, G, M, K, B (Megabytes, kilobytes, bytes)
+unit = re.findall(unit_regex, memory)[0]
+unit = Unit(unit)
 
 # Memory info
 mem = re.findall(mem_regex, memory)
 mem = int(''.join(str(e) for e in mem))
-
-# Unit M, K, B (Megabytes, kilobytes, bytes)
-unit = re.findall(unit_regex, memory)[0]
+match unit:  # Normalize size unit to KB
+  case UNIT.MB:
+    mem *= Multiplier.MB.value
+  case UNIT.GB:
+    mem *= Multiplier.GB.value
+  case UNIT.TB:
+    mem *= Multiplier.TB.value
 
 # PID
-pid = ''.join(re.findall(mem_regex, pid))
+pid = int(''.join(re.findall(mem_regex, pid)))
 
 # print(mem, unit, pid)
 print("==========RUNNING=============")
 # If an undesired process is found, we get rid of it
-if unit == UNIT and mem >= THRESHOLD:
-  print("Found mem. leak process! Killing " + pid)
+if mem >= THRESHOLD:
+  print("Found memory-leaking process! Killing process \"{}\" (pid={} mem={}{}>{}{}).".format(
+    pname, pid,
+    mem/Multiplier[unit.name].value, unit.name,
+    THRESHOLD/Multiplier[UNIT.name].value, UNIT.name
+  ))
   runUntilKilled(pid)
 else:
-  print("No mem. leak processes found")
+  print("No memory-leaking processes found.")
